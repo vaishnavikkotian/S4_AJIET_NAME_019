0b6ffc39bc1cd016f70b3ea8b1a3c47b4301f0fc
Vaishnavi K Kotian <vaishnavikkotian@gmail.com>
2026-05-08 15:45:53 +0530

    Create strdemo.java

---
+++ strdemo.java
@@ -0,0 +1,14 @@
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String gk = "Good Morning";
+        System.out.println(gk.charAt(8));
+
+        String a = "Hi";
+        String ab = "Hai";
+
+        System.out.println(a.equals(ab));
+
+        System.out.println(a + " " + ab);
+    }
+}
