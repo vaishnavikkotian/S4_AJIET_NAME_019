c700aadd09c80e0436063d9da7ecfa9ed7a6a6a9
Vaishnavi K Kotian <vaishnavikkotian@gmail.com>
2026-05-08 14:42:34 +0530

    Create qwer.java

---
+++ qwer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Creating an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Printing array elements
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+    }
+}
